2a25e1a3d8eb2b8bc33d0c127dd351b128f9c4c1
Ryan Srofe <ryan.srofe@formidable.com>
2022-06-23 11:53:09 -0400

    add React Native Owl SVG and ref in index

--- src/assets/featuredLogos/index.js
+++ src/assets/featuredLogos/index.js
@@ -3,11 +3,13 @@ import victory from "./victory.svg";
 import spectacle from "./spectacle.svg";
 import urql from "./urql.svg";
 import nuka from "./nuka.svg";
+import owl from "./owl.svg";
 
 export default {
   renature,
   victory,
   spectacle,
   urql,
-  nuka
+  nuka,
+  owl,
 };
