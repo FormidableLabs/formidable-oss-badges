6a6c825e86e7dd35ed28f831a2225c7400efea98
Ryan Srofe <ryan.srofe@formidable.com>
2022-06-23 11:56:57 -0400

    add small logos with icon only no text
    these svg files have been compressed leaving classNames intact, also added role and aria-labelledby attributes

---
+++ src/assets/smallLogos/index.js
@@ -0,0 +1,15 @@
+import renature from "./renature.svg";
+import victory from "./victory.svg";
+import spectacle from "./spectacle.svg";
+import urql from "./urql.svg";
+import nuka from "./nuka.svg";
+import owl from "./owl.svg";
+
+export default {
+  renature,
+  victory,
+  spectacle,
+  urql,
+  nuka,
+  owl,
+};
