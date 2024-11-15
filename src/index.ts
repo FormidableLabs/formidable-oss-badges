52ce32af5066f3157106230dd2f18f923d2db51f
Bryant Kelley <bryant@bryantkelley.com>
2024-11-15 14:51:24 -0800

    Featured Badge Names Type (#97)
    * Add FeaturedBadgeNames type
    
    * Remove string array of featured badge names
    
    * Add changeset

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export { default as ProjectBadge } from "./ProjectBadge"
 export { default as FeaturedBadge } from "./FeaturedBadge"
 export * from "./featuredProjectBadges"
 
-export const featuredBadgeNames = Object.keys(featuredLogos.default)
+export type FeaturedBadgeNames = keyof typeof featuredLogos.default
