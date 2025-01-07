683fcf826ac295b3f86278b2388e8e8983f00a5e
JDMathew <justindavidmathew@gmail.com>
2025-01-07 14:51:07 -0800

    fix type export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { FeaturedBadgeName } from "./FeaturedBadge"
-
 export { default as ProjectBadge } from "./ProjectBadge"
-export { default as FeaturedBadge } from "./FeaturedBadge"
-export { FeaturedBadgeName }
+export {
+  default as FeaturedBadge,
+  type FeaturedBadgeName,
+} from "./FeaturedBadge"
 export * from "./featuredProjectBadges"
